922339e32e4215c92f21b383e8089e41a777eff5
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-02-03 22:04:13 +0530

    feat: auth page progress

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import AdminHeader from "@widgets/Dashboard/components/AdminHeader";
+import AdminSidebar from "@widgets/Dashboard/components/AdminSidebar";
+import { useEffect } from "react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+          <div className="flex items-center flex-row w-screen">
+            <AdminSidebar />
+            <div className="flex flex-col w-full">
+              <AdminHeader />
+              <main className="min-h-[100vh] h-auto rounded-[5px] pt-[15vh] pl-[19vw] pr-[1vw] pb-[1vw] w-[99.5vw] flex  bg-red-50 bg-opacity-45">
+                <div className="w-full h-[82vh] relative overflow-auto">
+                  {children}
+                </div>
+              </main>
+            </div>
+          </div>
+  );
+}
